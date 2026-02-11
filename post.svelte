3878d706531c460235279852ef5e61c90c68c45a
raster <pxlskye@gmail.com>
2026-02-11 03:12:47 +0000

    some ui

---
+++ post.svelte
@@ -0,0 +1,38 @@
+    <div>
+        <div id="user" class="meta"></div>
+            <div id="time" class="meta"></div>
+        <div id="post" class="post"></div>
+    </div>
+    <style>
+        :root {
+  --black: #0a0a0a;
+  --white: #ffffff;
+}
+body {
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  max-width: 600px;
+  text-align: center;
+  justify-content: center;
+  vertical-align: middle;
+  height: 100vh;
+  margin: auto;
+  padding: 0;
+  font-family: Iowan Old Style, Apple Garamond, Baskerville, Times New Roman, Droid Serif, Times, Source Serif Pro, serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
+  color: var(--black);
+  background-color: var(--white)
+}
+a {
+  color: var(--black)
+}
+@media (prefers-color-scheme: dark) {
+  body {
+    color: var(--white);
+    background-color: var(--black);
+  }
+  a {
+    color: var(--white)
+  }
+}
+    </style>
